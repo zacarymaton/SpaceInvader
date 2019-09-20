07dd8e3e2caedcc97db3d7add22965473eac83a3
DESKTOP-7I6PF01\USUARIO <alvaro.zacary@gmail.com>
2019-09-20 07:21:35 -0400

    Controlador Principal MainActivity

--- app/src/main/java/com/example/graficos/spaceinvader/Controller/MainActivity.java
+++ app/src/main/java/com/example/graficos/spaceinvader/Controller/MainActivity.java
@@ -1,8 +1,10 @@
-package com.example.graficos.spaceinvader;
+package com.example.graficos.spaceinvader.Controller;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.example.graficos.spaceinvader.R;
 
 public class MainActivity extends AppCompatActivity {
 
